d32e5cc30cc714840d6fcddf674d7fde401c83c3
郝高明 <978235424@qq.com>
2017-12-20 18:01:31 +0800

    汉诺塔--递归

--- python1.py
+++ python1.py
@@ -173,4 +173,19 @@ def fact_iter(num, product):
 print(fact(10))
 #说明：Python解释器也没有做优化，所以，即使把上面的fact(n)函数改成尾递归方式，也会导致栈溢出。
 
+'''
+汉诺塔的移动可以用递归函数非常简单地实现。
+请编写move(n, a, b, c)函数，它接收参数n，表示3个柱子A、B、C中第1个柱子A的盘子数量，然后打印出把所有盘子从A借助B移动到C的方法。
+'''
+def move(n, a, b, c):
+    if n == 1:
+        print ('%s --> %s' % (a, c))
+    else:
+        move(n - 1, a, c, b)    # 简化问题
+        move(1, a, b, c)        # 移动最下面的大盘子
+        move(n - 1, b, a, c)
+
+if __name__ == '__main__':
+    move(3, 'A', 'B', 'C')
+
 
